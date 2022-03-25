f9c0bd55a486dbc82657bb855d102cc7ce9dbb8f
luthfidiqi <luthfidiqi@gmail.com>
2022-03-25 09:12:18 +0700

    update movie

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-Movie Ticket Backend
+# Beginer Backend
+
+## Collection Postman & Database
+
+- Collection Postman [[here](https://documenter.getpostman.com/view/20144301/UVsSN3i8)]
+- Database [[here](https://github.com/FazztrackWebClass/BeginerBackend/blob/main/tickitz.sql)]
+
+## References
+
+- Add Eslint Pretitier & Air Bnb [[here](https://dev.to/saurabhggc/add-eslint-prettier-and-airbnb-to-your-project-3mo8)]
+- SQL Injection in Node.js [[here](https://www.veracode.com/blog/secure-development/how-prevent-sql-injection-nodejs)]
+- Multiple Statement SQL [[here](https://stackoverflow.com/questions/23266854/node-mysql-multiple-statements-in-one-query)]
+
+## Installation
+
+- `npm i express body-parser cors morgan mysql2 helmet xss-clean compression`
+- `npm i nodemon -D`
+- `npx eslint --init`
+- `npm i eslint prettier eslint-config-prettier eslint-plugin-prettier -D`
+
+## Extensions
+
+- `Eslint` - Dirk Baeumer
+- `Path Intellisense` - Christian Kohler
+- `Prettier` - Prettier
+
+## Package
+
+- `express` [[here](https://www.npmjs.com/package/express)]
+- `body-parser` [[here](https://www.npmjs.com/package/body-parser)]
+- `cors` [[here](https://www.npmjs.com/package/cors)]
+- `morgan` [[here](https://www.npmjs.com/package/morgan)]
+- `mysql2` [[here](https://www.npmjs.com/package/mysql2)]
+- `helmet` [[here](https://www.npmjs.com/package/helmet)]
+- `xss-clean` [[here](https://www.npmjs.com/package/xss-clean)]
+- `compression` [[here](https://www.npmjs.com/package/compression)]
+- `nodemon` [[here](https://www.npmjs.com/package/nodemon)]
+
+## Modularisasi
+
+- Public = untuk menyimpan file upload / file yang nntinya bsa diakses secara public oleh user
+- Src = untuk menyimpan code dari project backend yang dibuat
+  - index.js = file root untuk mengkonfigurasi beberapa library/framework untuk nantinya digunakan di dalam project
+  - Config = untuk menyimpan konfigurasi
+  - Helpers = untuk menyimpan suatu fungsi yang bsa digunakan di file berbeda
+  - Routes = untuk menampung semua path route yang ada di dalam module / sebagai route navigation
+  - Modules = untuk mengelompokkan suatu fitur di dalam request
+    - Routes = menyimpan path dari tiap request / method
+    - Controller = Logic / menyimpan data hasil request / mengeluarkan response
+    - Model = database
--- src/modules/movie/movieController.js
+++ src/modules/movie/movieController.js
@@ -18,12 +18,27 @@ module.exports = {
   },
   getAllMovie: async (request, response) => {
     try {
-      const result = await movieModel.getAllMovie();
+      let { page, limit } = request.query;
+      page = Number(page); // 2
+      limit = Number(limit); // 3
+      const offset = page * limit - limit; // 2 * 3 - 3 = 3
+      const totalData = await movieModel.getCountMovie();
+      const totalPage = Math.ceil(totalData / limit);
+      const pageInfo = {
+        page,
+        totalPage,
+        limit,
+        totalData,
+      };
+
+      const result = await movieModel.getAllMovie(limit, offset);
+
       return helperWrapper.response(
         response,
         200,
         "Success get data !",
-        result
+        result,
+        pageInfo
       );
     } catch (error) {
       return helperWrapper.response(response, 400, "Bad Request", null);
@@ -31,6 +46,11 @@ module.exports = {
   },
   getMovieById: async (request, response) => {
     try {
+      // request = {
+      //   params: {
+      //     id: 1
+      //   }
+      // }
       const { id } = request.params;
       const result = await movieModel.getMovieById(id);
 
@@ -55,11 +75,18 @@ module.exports = {
   },
   createMovie: async (request, response) => {
     try {
+      const { name, category, synopsis } = request.body;
+      const setData = {
+        name,
+        category,
+        synopsis,
+      };
+      const result = await movieModel.createMovie(setData);
       return helperWrapper.response(
         response,
         200,
-        "Success get data !",
-        "Hello World"
+        "Success create data !",
+        result
       );
     } catch (error) {
       return helperWrapper.response(response, 400, "Bad Request", null);
@@ -68,11 +95,41 @@ module.exports = {
   updateMovie: async (request, response) => {
     try {
       const { id } = request.params;
+      const checkId = await movieModel.getMovieById(id);
+
+      if (checkId.length <= 0) {
+        return helperWrapper.response(
+          response,
+          404,
+          `Data by id ${id} not found`,
+          null
+        );
+      }
+
+      const { name, category, synopsis } = request.body;
+      const setData = {
+        name,
+        category,
+        synopsis,
+        updatedAt: new Date(Date.now()),
+      };
+
+      // eslint-disable-next-line no-restricted-syntax
+      for (const data in setData) {
+        // console.log(data); // property
+        // console.log(setData[data]); // value
+        if (!setData[data]) {
+          delete setData[data];
+        }
+      }
+
+      const result = await movieModel.updateMovie(id, setData);
+
       return helperWrapper.response(
         response,
         200,
-        "Success get data !",
-        "Hello World"
+        "Success update data !",
+        result
       );
     } catch (error) {
       return helperWrapper.response(response, 400, "Bad Request", null);
@@ -80,7 +137,11 @@ module.exports = {
   },
   deleteMovie: async (request, response) => {
     try {
-      const { id } = request.params;
+      // 1. tangkap id
+      // 2. proses pengecekan apakah id berada di dalam database
+      // 3. buat model dengan query = DELETE FROM movie where id = ?
+      // 4. resolve(id)
+      // 5. set response
       return helperWrapper.response(
         response,
         200,
