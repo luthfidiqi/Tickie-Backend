29a61cd54db36b0c52fa54110fa61c05ab5a2955
luthfidiqi <luthfidiqi@gmail.com>
2022-04-06 21:57:14 +0700

    auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -38,56 +38,9 @@ module.exports = {
       return helperWrapper.response(
         response,
         403,
-        "ONLY ADMIN THAT CAN DO THIS FEATURE"
+        "Only Admin That Can Do This Feature"
       );
     }
     next();
-  },
-  refresh: async (request, response) => {
-    try {
-      console.log(request.body);
-      const { refreshToken } = request.body;
-      const checkToken = await redis.get(`refreshToken:${refreshToken}`);
-      if (checkToken) {
-        return helperWrapper.response(
-          response,
-          403,
-          "Your refresh token cannot be use",
-          null
-        );
-      }
-      jwt.verify(refreshToken, "RAHASIABARU", async (error, result) => {
-        delete result.iat;
-        delete result.exp;
-        const token = jwt.sign(result, "RAHASIA", { expiresIn: "1h" });
-        const newRefreshToken = jwt.sign(result, "RAHASIABARU", {
-          expiresIn: "24h",
-        });
-        await redis.setEx(
-          `refreshToken:${refreshToken}`,
-          3600 * 48,
-          refreshToken
-        );
-        return helperWrapper.response(response, 200, "Success refresh token", {
-          id: result.id,
-          token,
-          refreshToken: newRefreshToken,
-        });
-      });
-    } catch (error) {
-      return helperWrapper.response(response, 400, "Bad Request", null);
-    }
-  },
-  logout: async (request, response) => {
-    try {
-      let token = request.headers.authorization;
-      const { refreshToken } = request.body;
-      token = token.split(" ")[1];
-      redis.setEx(`accessToken:${token}`, 3600 * 24, token);
-      redis.setEx(`refreshToken:${refreshToken}`, 3600 * 24, token);
-      return helperWrapper.response(response, 200, "Success logout", null);
-    } catch (error) {
-      return helperWrapper.response(response, 400, "Bad Request", null);
-    }
   },
 };
